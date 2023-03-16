627f6fc7847e420ec13ca59cc1d0abefb31416d2
FeridQarayev <tu7he6n9s@code.edu.az>
2023-03-17 01:47:48 +0400

    eslintignore & global d ts file

---
+++ client/src/components/base/navbar/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from './navbar.module.scss';
+
+function Navbar(): JSX.Element {
+  return <nav className={styled.home}>Navbar</nav>;
+}
+
+export default Navbar;
---
+++ client/src/global.d.ts
@@ -0,0 +1,6 @@
+declare module '*.css';
+declare module '*.scss';
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.svg';
+declare module '*.mp4';
