653c6e542a48633ae6dccba51570dbcbef308656
FeridQarayev <tu7he6n9s@code.edu.az>
2023-03-27 23:55:53 +0400

    npm react router dom

--- client/src/components/base/navbar/index.tsx
+++ client/src/components/base/navbar/index.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import styled from './navbar.module.scss';
+import style from './style.module.scss';
 
 function Navbar(): JSX.Element {
-  return <nav className={styled.home}>Navbar</nav>;
+  return (
+    <nav className={style.navbar}>
+      <div className={style.navbar__content}>
+        <div className={style.navbar__content__container}>
+          <div className={style.navbar__content__container__logo}>
+            <button className={style.navbar__content__container__logo__burger}>
+              <span></span>
+            </button>
+            <div className={style.navbar__content__container__logo__main}>
+              
+            </div>
+          </div>
+          <div className={style.navbar__content__container__elements}></div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
