5fca826aca997d4708271d38d0ceda4581981fce
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-03-30 22:55:45 +0400

    resolve fixed and svgs folder added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import Footer from './components/base/footer';
 
 function App(): JSX.Element {
-  return <div>
-  
-  </div>;
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/base/footer/index.tsx
+++ client/src/components/base/footer/index.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import footerImg from '../../../images/footerImg/Footer__img.png';
-import styles from './style.module.scss';
+import footer from '../../../images/footer/BackpackStory.png';
+import { ReactComponent  as FacebookSvg } from '../../../svgs/facebook.svg';
+import { ReactComponent as InstagramSvg } from '../../../svgs/instagram.svg';
+import {ReactComponent as PinterestSvg} from '../../../svgs/pinterest.svg';
+import { ReactComponent as TwitterSvg } from '../../../svgs/twitter.svg';
+import styles from './style.module.css';
+
+
 
 function Footer(): JSX.Element {
   return (
     <footer>
       <div className={styles.footer__left}>
         <div className={styles.first__footer__left__img}>
-          <img src={footerImg} className={styles.footer__left__img} />
+          <img src={footer} className={styles.footer__left__img} />
         </div>
       </div>
       <div className={styles.footer__middle}>
         <p>Copyright ©2023 All Rights Reserved </p>
       </div>
-      <div className={styles.footer__right}>
-        <div className={styles.footer__right__logo}>
-          <i className="fa-brands fa-facebook"></i>
-          <i className="fa-brands fa-twitter"></i>
-          <i className="fa-brands fa-instagram"></i>
-          <i className="fa-brands fa-pinterest"></i>
-        </div>
+      <div className={styles.footer__right}>   
+       <FacebookSvg/>
+       <InstagramSvg/>
+       <PinterestSvg/>
+       <TwitterSvg/>
       </div>
     </footer>
   );
