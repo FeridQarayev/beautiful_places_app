3b6cc639fb4beae9305a2840d7d2586089b2705d
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-04-02 14:33:20 +0400

    responsive changed

--- client/src/components/base/footer/index.tsx
+++ client/src/components/base/footer/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import footer from '../../../images/footer/BackpackStory.png';
-import { ReactComponent  as FacebookSvg } from '../../../svgs/facebook.svg';
+import { ReactComponent as FacebookSvg } from '../../../svgs/facebook.svg';
 import { ReactComponent as InstagramSvg } from '../../../svgs/instagram.svg';
-import {ReactComponent as PinterestSvg} from '../../../svgs/pinterest.svg';
+import { ReactComponent as PinterestSvg } from '../../../svgs/pinterest.svg';
 import { ReactComponent as TwitterSvg } from '../../../svgs/twitter.svg';
 import styles from './style.module.css';
-
-
 
 function Footer(): JSX.Element {
   return (
@@ -14,16 +12,15 @@ function Footer(): JSX.Element {
       <div className={styles.footer__left}>
         <div className={styles.first__footer__left__img}>
           <img src={footer} className={styles.footer__left__img} />
+          <p>Copyright ©2023 All Rights Reserved </p>
         </div>
       </div>
-      <div className={styles.footer__middle}>
-        <p>Copyright ©2023 All Rights Reserved </p>
-      </div>
-      <div className={styles.footer__right}>   
-       <FacebookSvg/>
-       <InstagramSvg/>
-       <PinterestSvg/>
-       <TwitterSvg/>
+
+      <div className={styles.footer__right}>
+        <FacebookSvg />
+        <InstagramSvg />
+        <PinterestSvg />
+        <TwitterSvg />
       </div>
     </footer>
   );
