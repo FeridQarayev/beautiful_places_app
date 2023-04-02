b21e036e14d7dc100541148297a81fa04bf12eca
Ferid <feridqarayev111@gmail.com>
2023-04-02 18:11:23 +0400

    footer added router

--- client/src/pages/base/BaseRoot.tsx
+++ client/src/pages/base/BaseRoot.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../../components/base/footer';
 import Navbar from '../../components/base/navbar';
 
 function BaseRoot(): JSX.Element {
@@ -6,6 +7,7 @@ function BaseRoot(): JSX.Element {
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
