4ef3d66bb9fa4c54bdcd61ea59212477c88a2d8d
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-04-08 21:11:36 +0400

    Login the title part is over

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import login from '../../images/footer/BackpackStory.png';
 import style from './style.module.scss';
 
 function Login(): JSX.Element {
-  return <div>Login</div>;
+  return <div id={style.login__bg__image}>
+    <div id={style.login}>
+       <aside>
+        <figure>
+          <a href="Home">
+          <img src={login} className={style.footer__left__img} />
+          </a>
+        </figure>
+       </aside>
+    </div>
+  </div>;
 }
 
 export default Login;
