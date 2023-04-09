131472d68dfbddf122d6cbc1237cd8eeb9c888a2
Ferid <feridqarayev111@gmail.com>
2023-04-09 12:40:28 +0400

    create component

---
+++ client/src/components/base/region-slider/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function RegionSlider(): JSX.Element {
+  return (
+    <section className="region_slider">
+      <div className="container">
+        
+      </div>
+    </section>
+  );
+}
+
+export default RegionSlider;
