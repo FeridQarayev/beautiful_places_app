c04239dee46101d94448cfb44384e5745d2d1b0e
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-04-09 21:54:26 +0400

    formik yup added

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,4 +1,6 @@
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import React from 'react';
+import * as Yup from 'yup';
 import login from '../../images/footer/BackpackStory.png';
 import { ReactComponent as EmailSvg } from '../../svgs/email.svg';
 import { ReactComponent as FacebookSvg } from '../../svgs/facebook-f.svg';
@@ -19,67 +21,87 @@ function Login(): JSX.Element {
             </a>
           </figure>
           <form>
-            <div className={style.access__social}>
-              <div className={style.social__context}>
-                <FacebookSvg />
-                <a href="https://www.facebook.com/" className={style.social__facebook}>
-                  Login with Facebook
-                </a>
-              </div>
-              <div className={style.social__context}>
-                <div className={style.social__context__svg}>
-                  <GooglePlusSvg />
-                </div>
-                <a href="https://www.google.com/" className={style.social__google}>
-                  Login with Google
-                </a>
-              </div>
-              <div className={style.social__context}>
-                <LinkedinSvg />
-                <a href="https://www.linkedin.com/" className={style.social__linkedin}>
-                  Login with Linkedin
-                </a>
-              </div>
-              <div className={style.divider}>
-                <span>Or</span>
-              </div>
-              <div className={style.form__group}>
-                <label>Email</label>
-                <input type="email" className={style.form__control} name="email"></input>
-                <EmailSvg />
-              </div>
-              <div className={style.form__group}>
-                <label>Password</label>
-                <input type="password" className={style.form__control} name="password"></input>
-                <LockSvg />
-                <button type="button" role="button" className={style.toggle__button}>
-                  Show
-                </button>
-              </div>
-            </div>
-            <div className={style.clearfix__left}>
-              <div className={style.clearfix__left__checkboxes}>
-                <label className={style.clearfix__left__checkboxes__check}>
-                  Remember me
-                  <input type="checkbox" checked />
-                  <span className={style.clearfix__left__checkboxes__checkmark}></span>
-                </label>
-              </div>
-              <div className={style.clearfix__right}>
-                <a id="forgot" href="#">
-                  Forget Password?
-                </a>
-              </div>
-              <a href="#" className={style.login__button}>
-                Login
-              </a>
-              <div className={style.new__login}>
-                New to Login?
-                <strong className={style.sign}>
-                  <a href="#"> Sign up!</a>
-                </strong>
-              </div>
-            </div>
+            <Formik
+              initialValues={{ email: '', password: '' }}
+              validationSchema={Yup.object({
+                email: Yup.string().email('Invalid email address').required('Required'),
+                password: Yup.string().min(6, 'Password must be at least 6 characters').required('Required'),
+              })}
+              onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+                }, 400);
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <div className={style.access__social}>
+                    <div className={style.social__context}>
+                      <FacebookSvg />
+                      <a href="https://www.facebook.com/" className={style.social__facebook}>
+                        Login with Facebook
+                      </a>
+                    </div>
+                    <div className={style.social__context}>
+                      <div className={style.social__context__svg}>
+                        <GooglePlusSvg />
+                      </div>
+                      <a href="https://www.google.com/"  className={style.social__google}>
+                        Login with Google
+                      </a>
+                    </div>
+                    <div className={style.social__context}>
+                      <LinkedinSvg />
+                      <a href="https://www.linkedin.com/" className={style.social__linkedin}>
+                        Login with Linkedin
+                      </a>
+                    </div>
+                    <div className={style.divider}>
+                      <span>Or</span>
+                    </div>
+                    <div className={style.form__group}>
+                      <label htmlFor="email">Email</label>
+                      <Field type="email" name="email" className={style.form__control} />
+                      <EmailSvg />
+                      <ErrorMessage name="email" component="div" className={style.error} />
+                    </div>
+                    <div className={style.form__group}>
+                      <label htmlFor="password">Password</label>
+                      <Field type="password" name="password" className={style.form__control} />
+                      <LockSvg />
+                      <button type="button" role="button" className={style.toggle__button}>
+                        Show
+                      </button>
+                      <ErrorMessage name="password" component="div" className={style.error} />
+                    </div>
+                  </div>
+                  <div className={style.clearfix__left}>
+                    <div className={style.clearfix__left__checkboxes}>
+                      <label className={style.clearfix__left__checkboxes__check}>
+                        <Field type="checkbox" name="rememberMe" />
+                        Remember me
+                        <span className={style.clearfix__left__checkboxes__checkmark}></span>
+                      </label>
+                    </div>
+                    <div className={style.clearfix__right}>
+                      <a id="forgot" href="#">
+                        Forget Password?
+                      </a>
+                    </div>
+                    <button type="submit" className={style.login__button} disabled={isSubmitting}>
+                      Login
+                    </button>
+                    <div className={style.new__login}>
+                      New to Login?
+                      <strong className={style.sign}>
+                        <a href="#"> Sign up!</a>
+                      </strong>
+                    </div>
+                  </div>
+                </Form>
+              )}
+            </Formik>
           </form>
           <div className={style.copy}>© Panagea</div>
         </aside>
