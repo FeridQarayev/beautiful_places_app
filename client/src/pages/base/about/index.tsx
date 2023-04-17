5b1657ea5bcf6493e59195d69686f8e3be3567fe
Ibrahimova11 <tu7uncctx@code.edu.az>
2023-04-17 23:16:45 +0400

    container content finished

--- client/src/pages/base/about/index.tsx
+++ client/src/pages/base/about/index.tsx
@@ -5,7 +5,13 @@ function Register(): JSX.Element {
   return (
     <section className={style.section}>
       <div className={style.section__bg}></div>
-      <div className={style.container}></div>
+      <div className={style.container}>
+        <div className={style.container__content}>
+            <h1>Azərbaycanda turizmin <span>dayanaqlı inkişafı strategiyası</span> </h1>
+            <p></p>
+            <p>Bu gün ölkədə dayanıqlılıq prinsiplərini tətbiq etdiyimiz layihələr və sahələrin sayı artmaqda davam edir. Dayanıqlı səyahət yanaşması nəinki təbiəti, xüsusilə endemik növləri mühafizə edir, həm də qlobal iqlim dəyişikliyi ilə mübarizə apararaq yerli icmalara dəstək verir, eləcə də turizm sənayesinin zərərli tullantılarını minimuma endirərək çirklənmənin azaldılmasında mühüm rol oynayır.</p>
+        </div>
+      </div>
     </section>
   );
 }
